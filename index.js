925989cff82489620251320328d56d91ecc798a3
nikki003 <nikkibhalodiya03@gmail.com>
2020-08-18 15:43:15 +0530

    created template for product overview

--- index.js
+++ index.js
@@ -13,30 +13,36 @@ const server = http.createServer((request, response) => {
         const pathName = url.parse(request.url, true).pathname;
         const id = url.parse(request.url, true).query.id;
         
+        // Product overview
         if(pathName === '/products' || pathName === '/') {
             response.writeHead(200, { 'Content-Type': 'text/HTML'});
-            response.end('This is the PRODUCTS page');
+
+            fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (error, data) => {
+                let overviewOutput = data;
+        
+                fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (error, data) => {
+                    let cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+                    overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+                    
+                    response.end(overviewOutput);
+                });
+            });
         }
+
+        // Laptop details
         else if(pathName === '/laptop' && id < laptopData.length) {
             response.writeHead(200, { 'Content-Type': 'text/HTML'});
             
             const laptop = laptopData[id];
 
             fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (error, data) => {
-                let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-                output = output.replace(/{%PRICE%}/g, laptop.price);
-                output = output.replace(/{%SCREEN%}/g, laptop.screen);
-                output = output.replace(/{%CPU%}/g, laptop.cpu);
-                output = output.replace(/{%IMAGE%}/g, laptop.image);
-                output = output.replace(/{%STORAGE%}/g, laptop.storage);
-                output = output.replace(/{%RAM%}/g, laptop.ram);
-                output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
-
+                const output = replaceTemplate(data, laptop);
                 response.end(output);
             });
 
         }
 
+        // URL not found
         else {
             response.writeHead(404, { 'Content-Type': 'text/HTML'});
             response.end('URL was not found on the server');
@@ -48,4 +54,18 @@ const server = http.createServer((request, response) => {
 
 server.listen(1337, '127.0.0.1', () => {
     console.log("Listening port 1337 of localhost");
-})
+});
+
+const replaceTemplate = (originalHtml, laptop) => {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+};
